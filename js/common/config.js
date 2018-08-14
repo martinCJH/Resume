65724684f4fe69944744bd7efa75965fac196bc5
Toasmo <gtb1116@gmail.com>
2018-08-14 17:10:31 +0800

    change

--- js/common/config.js
+++ js/common/config.js
@@ -9,7 +9,7 @@ var CF_COMMIT_ID = '';
 
 // ------ page setting ------
 var CF_PAGE_COUNTDOWN = 5;                                   // 在報錯頁面中，設置返回首頁的讀秒 (單位：秒)
-var CF_DEFAULT_LANGUAGE = 'en';                              // 預設語言 (en, ja, zh_TW)
+var CF_DEFAULT_LANGUAGE = 'zh_TW';                              // 預設語言 (en, ja, zh_TW)
 var CF_DEFAULT_MAP_ZOOM = 15;                                // GPS地圖、軌跡，default map顯示的放大倍率
 var CF_DEFAULT_POS = {lat: 25.00305, lng: 121.49153};        // GPS地圖、軌跡，default顯示的中心點
 
